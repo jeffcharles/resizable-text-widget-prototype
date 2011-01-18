f4803c380bf56620d96af89162a71a583897d0b3
Jeffrey Charles <jeffreycharles@gmail.com>
2011-01-18 13:32:41 -0500

    Change cursor when it reaches edge of TextNode

--- src/views/main.py
+++ src/views/main.py
@@ -38,12 +38,43 @@ class TextNode(wx.Panel):
         self.text_ctrl = wx.TextCtrl(self, style=wx.TE_MULTILINE)
         self.text_ctrl.Bind(wx.EVT_TEXT, self.OnTextChanged)
         self.sizer = wx.BoxSizer()
-        self.sizer.Add(self.text_ctrl, 1, wx.EXPAND)
+        self.sizer.Add(self.text_ctrl, 1, wx.ALL|wx.EXPAND, 5)
+        
+        self.Bind(wx.EVT_MOTION, self.OnMouseMotion)
         
         self.SetAutoLayout(True)
         self.SetSizer(self.sizer)
         self.Layout()
         
+    def OnMouseMotion(self, event):
+        xpos, ypos = event.GetPositionTuple()
+        p_width, p_height = self.GetSizeTuple()
+        
+        cursor_left = 0 < xpos <= 5
+        cursor_right = 0 < (p_width - xpos) <= 5
+        cursor_top = 0 < ypos <= 5
+        cursor_bottom = 0 < (p_height - ypos) <= 5
+        
+        if cursor_left and cursor_top:
+            cursor = wx.CURSOR_SIZENWSE
+        elif cursor_right and cursor_top:
+            cursor = wx.CURSOR_SIZENESW
+        elif cursor_left and cursor_bottom:
+            cursor = wx.CURSOR_SIZENESW
+        elif cursor_right and cursor_bottom:
+            cursor = wx.CURSOR_SIZENWSE
+        elif cursor_left:
+            cursor = wx.CURSOR_SIZEWE
+        elif cursor_right:
+            cursor = wx.CURSOR_SIZEWE
+        elif cursor_top:
+            cursor = wx.CURSOR_SIZENS
+        elif cursor_bottom:
+            cursor = wx.CURSOR_SIZENS
+        else:
+            cursor = wx.CURSOR_ARROW
+        self.SetCursor(wx.StockCursor(cursor))
+    
     def OnTextChanged(self, event):
         self._controller.text_changed(self._text_note, event.GetEventObject().Value)
 
